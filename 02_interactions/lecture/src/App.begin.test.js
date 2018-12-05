f574cce701919e3b25344a8760111f8746f0f55f
Andrew Smith <andrewsmith@alumni.stanford.edu>
2018-12-05 13:01:26 -0800

    More nits.

--- 02_interactions/lecture/src/App.begin.test.js
+++ 02_interactions/lecture/src/App.begin.test.js
@@ -4,10 +4,13 @@ import { App } from './App'
 
 /**
  * Testing Objectives:
- * - [] clicking the Follow button increments the number of followers and changes button text to "Unfollow"
- * - [] clicking the "Unfollow" button decrements the number of followers and changes button text to "Follow"
+ * - [] clicking the Follow button increments the number of
+ *      followers and changes button text to "Unfollow"
+ * - [] clicking the "Unfollow" button decrements the number of
+ *      followers and changes button text to "Follow"
  * - [] button to send a message is initially disabled
- * - [] typing into the message field and clicking to send the message will display a confirmation message
+ * - [] typing into the message field and clicking to send the
+ *      message will display a confirmation message
  */
 
 describe('App', () => {
